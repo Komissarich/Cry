77cdf5fd2ad4566cc935d9d7a7e6be5e631821c0
komissarich <egorkart1@gmail.com>
2024-10-17 14:48:17 +0300

    try

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+
+fun function(lst: MutableList<Int>) {
+    for (i
+
+    in lst) {
+        println(i)
+    }
+}
+fun main() {
+    val message = "aaaa"
+    var x:String = "AAAAA"
+    x = "abcd"
+    var lst = mutableListOf<Int>(1,2,3,4)
+    function(lst)
+    println(lst)
+}
